3e617931df60e601f2f5c2b369611283b94af394
LamaV10 <lama.customs@gmail.com>
2025-04-06 19:23:18 +0200

    Cleanup all the unnecessary stuff from the usage of the delay.

--- timer.py
+++ timer.py
@@ -3,19 +3,13 @@ import time
 import keyboard
 
 pressed = False
-# delay = 2 
-# delayTime = 0
-fortnite = 0
+printCount = 0
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
 def timer():
     global pressed 
-    # global delay
-    # global delayTime
-    global fortnite
-
-    # currentTime = time.time()
+    global printCount
 
     if keyboard.read_key() == "space" and pressed == False:
         startTime = time.time()
@@ -24,9 +18,9 @@ def timer():
     if keyboard.read_key() == "space" and pressed == True: 
         endTime = time.time()
         pressed = False
-        if fortnite % 2 == 0: 
+        if printCount % 2 == 0: 
             print(round(endTime - startTime, 2))
-        fortnite += 1
+        printCount += 1
 
 
 while True:
