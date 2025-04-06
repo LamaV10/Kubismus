08289220ae5227bc15d3d4c3eebee0abec5e46d3
LamaV10 <lama.customs@gmail.com>
2025-04-06 14:53:15 +0200

    Bug fix with starting and stopping the timer => used to stop and start at the same time.

--- timer.py
+++ timer.py
@@ -2,18 +2,34 @@ import time
 import keyboard
 
 pressed = False
+finished = False
+delay = 2 
+delayTime = 0
 
 def timer():
     global pressed 
+    global finished
+    global delay
+    global delayTime
 
-    if keyboard.read_key() == "space" and pressed == False:
-        startTime = time.time()
-        pressed = True
+    currentTime = time.time()
 
-    if keyboard.read_key() == "space" and pressed == True:
+    if delay + delayTime <= currentTime: 
+        if keyboard.read_key() == "space" and pressed == False:
+            startTime = time.time()
+            pressed = True
+        delayTime = time.time()
+
+    if keyboard.read_key() == "space" and pressed == True: 
         endTime = time.time()
         print(round(endTime - startTime, 2))
         pressed = False
+        finished = True
+
+    if finished == True:
+        startTime = 0
+        endTime = 0
+        finished = False
 
 while True:
     timer()
