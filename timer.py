bcd088d76163a8e6ac51f7fbbf006fc9833a713b
LamaV10 <lama.customs@gmail.com>
2025-04-08 16:39:46 +0200

    Added comments.

--- timer.py
+++ timer.py
@@ -4,22 +4,25 @@ import keyboard
 
 pressed = False
 printCount = True 
-
+# clears the terminal
 os.system('cls' if os.name == 'nt' else 'clear')
 
 print("Press space to start and then again to stop!")
-
+# main timer function
 def timer():
     global pressed 
     global printCount
 
+    # start the timer
     if keyboard.read_key() == "space" and pressed == False:
         startTime = time.time()
         pressed = True
 
+    # stop the timer
     if keyboard.read_key() == "space" and pressed == True: 
         endTime = time.time()
         pressed = False
+        # every second output is being printed => otherwise would print the time between every space press
         if printCount == True: 
             print(round(endTime - startTime, 2))
             printCount = False 
