7ea0e3570958796389674a1e862aae24b1677862
LamaV10 <lama.customs@gmail.com>
2025-04-12 17:26:14 +0200

    Removed some unnecessary stuff from writing things into a file. Timer does not work right for now.

--- timer.py
+++ timer.py
@@ -27,25 +27,22 @@ def timer():
         pressed = False
         totalTime = round(endTime - startTime, 2)
 
-        # Writing to an existing file (content will be overwritten)
-        with open("times.txt", "w") as f:
-            f.write("Written to the file.")
-
-        f = open("times.txt","r")
-        print(f.read())
-
-        # Writing multiple lines to an existing file using writelines()
-        s = ["time: ", numpy.format_float_positional(totalTime), "s"]
-
-        with open("times.txt", "w") as f:
-            f.writelines(s)
-
         # every second output is being printed => otherwise would print the time between every space press
         if printCount == True: 
             print("Time: ", totalTime, "s")
             printCount = False 
+
+            # Writing multiple lines to an existing file using writelines()
+            timeString = numpy.format_float_positional(totalTime)
+            sString = "s"
+
+            with open("times.txt", "w") as f:
+                f.write(timeString)
+                f.write("\n")
         else:
             printCount = True 
+
+
 
 # main loop
 while True:
