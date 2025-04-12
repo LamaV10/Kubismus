52365956fe400693e8c790b0ac2448f53881aa10
LamaV10 <lama.customs@gmail.com>
2025-04-12 13:01:32 +0200

    Now writes the time to the file, but it overriedes the old entry everytime.

--- timer.py
+++ timer.py
@@ -1,3 +1,4 @@
+import numpy
 import os
 import time
 import keyboard
@@ -25,6 +26,20 @@ def timer():
         endTime = time.time()
         pressed = False
         totalTime = round(endTime - startTime, 2)
+
+        # Writing to an existing file (content will be overwritten)
+        with open("times.txt", "w") as f:
+            f.write("Written to the file.")
+
+        f = open("times.txt","r")
+        print(f.read())
+
+        # Writing multiple lines to an existing file using writelines()
+        s = ["time: ", numpy.format_float_positional(totalTime), "s"]
+
+        with open("times.txt", "w") as f:
+            f.writelines(s)
+
         # every second output is being printed => otherwise would print the time between every space press
         if printCount == True: 
             print("Time: ", totalTime, "s")
